e8eb2595382e6a27089042ccefd60ab9c12d4c72
Dejan Karamanović <dejankaramanovic@gmail.com>
2025-01-21 23:39:28 +0100

    4.1 Changed background color to dark gray, added embeded yt and souncloud playlists, added fixed footer, floating arrow to TOP of the page, and minor css ajustment.

--- 100 Push-Ups Terminator programm/4.0 w added Serbian Language/script.js
+++ 100 Push-Ups Terminator programm/4.0 w added Serbian Language/script.js
@@ -213,9 +213,20 @@ document.addEventListener("DOMContentLoaded", () => {
     displayTime.innerHTML = "00:00:00";
   }
 
-  function scrollToTop() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+ // Scroll to the top function
+function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+// Show/hide the arrow-up button based on scroll position
+window.addEventListener('scroll', function () {
+  const arrowUp = document.getElementById('arrow-up');
+  if (window.scrollY > 100) {
+    arrowUp.classList.add('show'); // Add class to make it visible
+  } else {
+    arrowUp.classList.remove('show'); // Remove class to hide it
   }
+});
     
     
   function toggleLanguage() {
